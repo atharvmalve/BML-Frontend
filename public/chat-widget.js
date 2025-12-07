a92c87c99b86b070712593014a61f6ed31f1067e
atharvmalve <atharvmalve.student@gmail.com>
2025-12-07 14:23:44 +0530

    widgett

--- public/chat-widget.js
+++ public/chat-widget.js
@@ -1,30 +1,9 @@
-class ChatWidget extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = `
-      <style>
-        .chat-frame {
-          position: fixed;
-          bottom: 20px;
-          right: 20px;
-          width: 380px;
-          height: 520px;
-          border: none;
-          z-index: 999999;
-        }
-
-        @media (max-width: 768px) {
-          .chat-frame {
-            width: 90vw;
-            height: 80vh;
-            bottom: 10px;
-            right: 10px;
-          }
-        }
-      </style>
-
-      <iframe class="chat-frame" src="/"></iframe>
-    `;
-  }
-}
-
-customElements.define("chat-widget", ChatWidget);
+(function () {
+  const script = document.createElement("script");
+  script.src = "https://bml-frontend-eta.vercel.app/chat-widget.js";
+  script.onload = () => {
+    const widget = document.createElement("chat-widget");
+    document.body.appendChild(widget);
+  };
+  document.head.appendChild(script);
+})();
