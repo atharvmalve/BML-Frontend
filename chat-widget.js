e0b0d209d9e1a8ba166b2b3136990be757930da6
atharvmalve <atharvmalve.student@gmail.com>
2025-12-07 14:00:04 +0530

    added widget

---
+++ chat-widget.js
@@ -0,0 +1,30 @@
+class ChatWidget extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <style>
+        .chat-frame {
+          position: fixed;
+          bottom: 20px;
+          right: 20px;
+          width: 380px;
+          height: 520px;
+          border: none;
+          z-index: 999999;
+        }
+
+        @media (max-width: 768px) {
+          .chat-frame {
+            width: 90vw;
+            height: 80vh;
+            bottom: 10px;
+            right: 10px;
+          }
+        }
+      </style>
+
+      <iframe class="chat-frame" src="https://bml-frontend-eta.vercel.app/index.html"></iframe>
+    `;
+  }
+}
+
+customElements.define("chat-widget", ChatWidget);
